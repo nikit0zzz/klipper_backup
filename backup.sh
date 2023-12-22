2cf3aa5fce602e1d3ccf6f1bf7d90fe8031055e5
klipper <klipper backup>
2023-12-23 01:02:42 +0300

    klipper backup

---
+++ backup.sh
@@ -0,0 +1,19 @@
+!#/usr/bin/bash
+mkdir -p $HOME/klipper_backup/printer/config $HOME/klipper_backup/printer/macros $HOME/klipper_backup/printer/timelapse $HOME/klipper_backup/printer/klipper_macro $HOME/klipper_backup/os
+cat $HOME/moonraker-timelapse/klipper_macro/timelapse.cfg > $HOME/klipper_backup/printer/klipper_macro/timelapse.cfg
+cat $HOME/printer_data/config/telegram.conf > $HOME/klipper_backup/printer/config/telegram.cfg
+cat $HOME/printer_data/config/printer.conf > $HOME/klipper_backup/printer/config/printer.cfg
+cat $HOME/printer_data/config/moonraker.conf > $HOME/klipper_backup/printer/config/moonraker.cfg
+cat $HOME/printer_data/config/crowsnest.conf > $HOME/klipper_backup/printer/config/crowsnest.cfg
+cd $HOME/printer_data/config/macros
+for cfg in *.cfg
+  do
+    cat $cfg > $HOME/klipper_backup/printer/macros/$cfg
+  done
+
+apt list > $HOME/klipper_backup/os/apt.list
+
+cd $HOME/klipper_backup
+git add --all
+git commit -m "klipper backup"
+git push
