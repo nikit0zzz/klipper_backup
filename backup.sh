2e99e21e3158104e70b97bf2fb71ab2310e62374
klipper <klipper>
2024-08-13 15:00:11 +0300

    klipper backup

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 !#/usr/bin/bash
-mkdir -p $HOME/klipper_backup/printer/config $HOME/klipper_backup/printer/macros $HOME/klipper_backup/printer/timelapse $HOME/klipper_backup/printer/klipper_macro $HOME/klipper_backup/os $HOME/klipper_backup/spoolman $HOME/printer_backup/database
+mkdir -p $HOME/klipper_backup/printer/config $HOME/klipper_backup/printer/macros $HOME/klipper_backup/printer/timelapse $HOME/klipper_backup/printer/klipper_macro $HOME/klipper_backup/os $HOME/klipper_backup/spoolman $HOME/klipper_backup/database
 #cat $HOME/moonraker-timelapse/klipper_macro/timelapse.cfg > $HOME/klipper_backup/printer/klipper_macro/timelapse.cfg
 #cat $HOME/printer_data/config/telegram.conf > $HOME/klipper_backup/printer/config/telegram.cfg
 #cat $HOME/printer_data/config/printer.cfg > $HOME/klipper_backup/printer/config/printer.cfg
