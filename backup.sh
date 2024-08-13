9378384ef8002c387a6f43a4e41d7488b2e8af37
klipper <klipper>
2024-08-13 13:00:11 +0300

    klipper backup

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 !#/usr/bin/bash
-mkdir -p $HOME/klipper_backup/printer/config $HOME/klipper_backup/printer/macros $HOME/klipper_backup/printer/timelapse $HOME/klipper_backup/printer/klipper_macro $HOME/klipper_backup/os
+mkdir -p $HOME/klipper_backup/printer/config $HOME/klipper_backup/printer/macros $HOME/klipper_backup/printer/timelapse $HOME/klipper_backup/printer/klipper_macro $HOME/klipper_backup/os $HOME/klipper_backup/spoolman $HOME/printer_backup/database
 #cat $HOME/moonraker-timelapse/klipper_macro/timelapse.cfg > $HOME/klipper_backup/printer/klipper_macro/timelapse.cfg
 #cat $HOME/printer_data/config/telegram.conf > $HOME/klipper_backup/printer/config/telegram.cfg
 #cat $HOME/printer_data/config/printer.cfg > $HOME/klipper_backup/printer/config/printer.cfg
@@ -20,6 +20,10 @@ for cfg in *.cfg
   done
 
 apt list > $HOME/klipper_backup/os/apt.list
+
+cat $HOME/printer_data/moonraker.asvc > $HOME/klipper_backup/moonraker.asvc 
+cat $HOME/klipper/printer_data/database/moonraker-sql.db|base64 > $HOME/klipper_backup/database/moonraker-sql.db.base64
+cat $HOME/.local/share/spoolman/spoolman.db|base64 > $HOME/klipper_backup/spoolman/spoolman.db.base64
 
 cd $HOME/klipper_backup
 git add --all
